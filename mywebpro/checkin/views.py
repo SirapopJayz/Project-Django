29c347c6c6c48c65e97008ccc0788304e6ebb6e4
sirapopjayz <jayza130443@gmail.com>
2020-02-01 16:52:42 +0700

    -

--- mywebpro/checkin/views.py
+++ mywebpro/checkin/views.py
@@ -1,14 +1,14 @@
-from django.http import HTTPResponse
+from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def checkteachid(request, teacher):
-    return HTTPResponse("This Subject is teach by %s.", % teacher)
+    return HttpResponse("This Subject is teach by %s." % teacher)
 
 
 def checkcourse(request, sub, amt, stu, abt):
-    return HTTPResponse("Subject = %s amount = %d studying = %d Absent = %d " % (sub, amt, stu, abt))
+    return HttpResponse("Subject = %s amount = %d studying = %d Absent = %d " % (sub, amt, stu, abt))
 
 
 def checkin(request, qrCode):
-    return HTTPResponse("Scan QR CODE Here %s." % qrCode)
+    return HttpResponse("Scan QR CODE Here %s." % qrCode)
